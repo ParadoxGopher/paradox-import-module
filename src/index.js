27db93543bf26872077a96c2c62993159a19d065
ParadoxGery <7468413+ParadoxGery@users.noreply.github.com>
2020-11-15 16:19:18 +0100

    enable actors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import items from "./items.js"
 import monster from "./monster.js"
+import actors from "./actors.js"
 import settings from "./settings.js"
 
 Hooks.once("ready", init)
-
-const RequestEventType = "paradox-import:request"
-const ResponseEventType = "paradox-import:response"
 
 function init() {
     settings()   
 
     items()
-    monster()
+	monster()
+	actors()
 }
